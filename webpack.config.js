f5a514d331c76b0f6d192895902a9e7a258b22dd
Kevin <mr.kev@me.com>
2018-06-26 02:00:52 -0400

    build and fix docs

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,27 @@
 const path = require('path');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
-module.exports = {
+const output = {
+  path: path.resolve(__dirname, 'dist'),
+  filename: 'fnMatch.js',
+  library: 'fnMatch',
+  libraryTarget: 'umd',
+}
+
+const config = {
   entry: './src/index.js',
   plugins: [
     new UglifyJSPlugin(),
   ],
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'fnMatch.js',
-    library: 'fnMatch',
-    libraryTarget: 'umd',
-  },
+  output,
   mode: 'production',
 };
+
+module.exports = [
+  config,
+  { ...config,
+    output: { ...output,
+      path: path.resolve(__dirname, 'docs', 'js')
+    }
+  }
+]
