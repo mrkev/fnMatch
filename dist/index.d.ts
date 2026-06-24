dcf3b3d3fab73bf35e4888e6eb6dcee09b8919cd
Kevin Chavez <mr.kev@me.com>
2026-06-24 07:07:01 -0600

    improve types

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,9 +1,5 @@
-declare type ExtractReturnTypes<T extends ((...args: any[]) => any)[]> = [
-    ...{
-        [K in keyof T]: T[K] extends (...args: any[]) => infer R ? R : never;
-    }
-];
-declare type MatchResult<T extends ((...args: any[]) => any)[]> = ExtractReturnTypes<T>[number];
-export declare const match: <VL>(v: VL) => <CS extends ((arg: any) => any)[]>(...cases: CS) => MatchResult<CS> | undefined;
-export declare const func: <CS extends ((arg: any) => any)[]>(...cases: CS) => <VL>(v: VL) => MatchResult<CS> | undefined;
+type AnyCase = (arg: any) => any | (() => any);
+type MatchResult<CS extends AnyCase[]> = [] extends Required<Parameters<CS[number]>> ? ReturnType<CS[number]> : ReturnType<CS[number]> | undefined;
+export declare function match<VL>(v: VL): <CS extends AnyCase[]>(...cases: CS) => MatchResult<CS>;
+export declare function func<T extends AnyCase, CS extends AnyCase[] = T[]>(...cases: CS): <VL>(v: VL) => MatchResult<CS>;
 export {};
